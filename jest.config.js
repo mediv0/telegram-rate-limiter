8aaac3943ea1be0ce2878da0140c7682adf7272f
mediv0 <anonymousaxis@gmail.com>
2022-03-30 16:23:49 +0430

    chore: config files

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    restoreMocks: true,
+    clearMocks: true,
+    resetMocks: true,
+};
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.ts",
+    mode: "production",
+    externals: {
+        redis: "redis",
+        "@types/redis": "@types/redis",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        filename: "limiter.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+};
